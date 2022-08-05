84e9512dffdb176d43ab5a895c544aa07556380a
Timofey solovov <timoshA228@228.com>
2022-08-05 19:05:31 +0300

    ...

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTAwNDQ3MDIxNjkxNjQ3MTg5MA.Gh3obY.ijzywauL2b94gGqDYKuSMmSO89d7nefYnWHn2w',
+    'token': 'your token',
     'bot': 'zxcBotyanya',
     'id': 1004470216916471890,
     'prefix': '!'
